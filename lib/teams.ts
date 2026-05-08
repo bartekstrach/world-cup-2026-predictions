e59add3b3aeccdcf068992aace9d15335b951fd6
Bartek Strach <iambartekstrach@gmail.com>
2026-05-08 23:58:23 +0200

    feat: fifa - iso country codes

--- lib/teams.ts
+++ lib/teams.ts
@@ -1,6 +1,21 @@
 import { TEAMS_SEPARATOR } from "./constants";
 import { getCountryFlag, getCountryName } from "./country-utils";
 
+const ISO_TO_FIFA_CODE: Record<string, string> = {
+  DEU: "GER",
+  HRV: "CRO",
+  CHE: "SUI",
+  NLD: "NED",
+  PRT: "POR",
+  SAU: "KSA",
+  ZAF: "RSA",
+  URY: "URU",
+  DZA: "ALG",
+};
+
+const getDisplayTeamCode = (teamCode: string) =>
+  ISO_TO_FIFA_CODE[teamCode.toUpperCase()] || teamCode;
+
 export const getShortMatchTeamNames = ({
   displayFlags,
   homeTeamCode,
@@ -11,11 +26,13 @@ export const getShortMatchTeamNames = ({
   awayTeamCode: string;
 }) => {
   const chunks: string[] = [];
+  const displayHomeTeamCode = getDisplayTeamCode(homeTeamCode);
+  const displayAwayTeamCode = getDisplayTeamCode(awayTeamCode);
 
   if (displayFlags) chunks.push(getCountryFlag(homeTeamCode));
-  chunks.push(homeTeamCode);
+  chunks.push(displayHomeTeamCode);
   chunks.push(TEAMS_SEPARATOR);
-  chunks.push(awayTeamCode);
+  chunks.push(displayAwayTeamCode);
   if (displayFlags) chunks.push(getCountryFlag(awayTeamCode));
 
   return chunks.join(" ");
